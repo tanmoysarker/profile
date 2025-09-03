f3067f652b32d0cf72d69cab132570d84de081c6
Tanmoy Sarker <YOUR_NOREPLY>
2025-09-03 19:31:26 +1000

    Updates

--- components/UI/Elements/Blobs/Blobs.jsx
+++
@@ -1,8 +0,0 @@
-import styles from './Blobs.module.scss';
-
-export default function Blobs({classVariable, type}) {
-    const blobType = type ? type : 'v1';
-    return (
-        <div className={`${styles.blob} ${styles[blobType]} ${classVariable}`}></div>
-    )
-}
--- utils/utils.js
+++
@@ -1,7 +0,0 @@
-export const getCurrentYear = () => {
-    return new Date().getFullYear();
-};
-
-export const getRandomValue = (maxValue, minValue) => {
-    return Math.ceil(Math.random() * (maxValue - minValue) + minValue);
-}
